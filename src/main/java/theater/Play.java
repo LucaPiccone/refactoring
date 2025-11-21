2befeb11acefa3b75eb589fbb73ebb75815ed227
Luca Piccone <lpiccone19@gmail.com>
2025-11-21 15:50:38 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,27 @@
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ *
+ * @null This class does not accept null values for name or type.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
 }
